61909f144107de245db6e98d9f6ed4bfcf142ed6
Jan20199 <jan20199@email.vccs.edu>
2024-09-19 13:04:14 -0600

    first commit

---
+++ activity2.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const myFriendApples = 2;
+let total = myApples+ myFriendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = myFriendApples;
+document.getElementById("totalApplesElement").textContent = total;
